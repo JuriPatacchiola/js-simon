0aa173edb025387f5b42b8a60b76c53b3323e30c
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-16 15:00:56 +0200

    add the my variable

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+ // Creo le mie variabili
+    const numbersDiv = document.getElementById("numbers");
+    const timerDiv = document.getElementById("timer");
+    const inputsDiv = document.getElementById("inputs");
+    const resultDiv = document.getElementById("result");
