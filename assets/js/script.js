983a975bca9a810876f618156d69bb1a24ce3979
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-16 15:12:19 +0200

    add the generator number

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,10 @@
     const timerDiv = document.getElementById("timer");
     const inputsDiv = document.getElementById("inputs");
     const resultDiv = document.getElementById("result");
+
+ // Faccio genera 5 numeri casuali tra 1 e 100
+    const randomNumbers = Array.from({ length: 5 }, () =>
+      Math.floor(Math.random() * 100) + 1
+    );
+    numbersDiv.textContent = randomNumbers.join(" - ");
+    console.log("Numeri generati:", randomNumbers);
