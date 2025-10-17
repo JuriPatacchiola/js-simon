a7c54c9fa37035bb5267aad5264f1dec1fde18ea
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-17 10:01:45 +0200

    add the mat random and to ceak if the number is reeat or not

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,19 @@ let numeriCasuali = [];
 let tempo = 30;
 let intervallo;
 
+
+// Genera 5 numeri casuali diversi
+function generaNumeri() {
+  numeriCasuali = [];
+
+  for (let i = 0; i < 5; i++) {
+    let nuovoNumero = Math.floor(Math.random() * 99) + 1;
+
+    // Se il numero è già presente, rigeneralo
+    if (numeriCasuali.includes(nuovoNumero)) {
+      i = i - 1; // torno indietro di uno
+    } else {
+      numeriCasuali.push(nuovoNumero);
+    }
+  }
+}
